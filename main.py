a95ed5f7b2177a7608c2d05cd0f8b92481ac44c5
maniswaroopbachi-oss <maniswaroop.bachi@gmail.com>
2025-12-07 18:01:19 +0530

    Update main.py model 2.0

--- main.py
+++ main.py
@@ -42,13 +42,14 @@ Term: {data.question}
 
 
     try:
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-2.0-flash")
         response = model.generate_content(prompt)
 
         return {"answer": response.text}
 
     except Exception as e:
         return {"error": str(e)}
+
 
 
 
